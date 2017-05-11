b7311db6e1b6179ca3d9f2ecfd87afd4a3329454
Petter Walbø Johnsgård <petter@dekode.no>
2017-05-11 20:35:50 +0200

    Offer the user to run on a different port if default port is busy

--- config/browsersync.config.js
+++ config/browsersync.config.js
@@ -7,27 +7,30 @@ const removeIfManifestFile = require( './utils/manifest-remove-file' );
 /**
  * Config
  */
-module.exports = {
-	host: 'localhost',
-	port: 3000,
-	proxy: {
-		target: require( paths.appPackageJson ).proxy,
-	},
-	rewriteRules: [{
-		// Remove all manifest js files
-		match: /<script.*src=(?:['"])(.*)(?:['"]).*><\/script>/gi,
-		fn: removeIfManifestFile,
-	}, {
-		// Remove all manifest css files
-		match: /<link.*href=(?:['"])(.*)(?:['"]).*>/gi,
-		replace: removeIfManifestFile,
-	}, {
-		// Append bundle
-		match: /<\/body>/i,
-		replace: ( req, res, match ) =>
-			`<script type="text/javascript" src="http://localhost:3100/bundle.js"></script>\n${match}`,
-	}],
-	codeSync: false,
-	timestamps: false,
-	logLevel: 'silent',
+module.exports = ( port, devPort ) => {
+	return {
+		host: 'localhost',
+		port,
+		proxy: {
+			target: require( paths.appPackageJson ).proxy,
+		},
+		rewriteRules: [{
+			// Remove all manifest js files
+			match: /<script.*src=(?:['"])(.*)(?:['"]).*><\/script>/gi,
+			fn: removeIfManifestFile,
+		}, {
+			// Remove all manifest css files
+			match: /<link.*href=(?:['"])(.*)(?:['"]).*>/gi,
+			replace: removeIfManifestFile,
+		}, {
+			// Append bundle
+			match: /<\/body>/i,
+			replace: ( req, res, match ) =>
+				`<script type="text/javascript">var heisenbergDevPort = ${devPort};</script>\n` +
+				`<script type="text/javascript" src="http://localhost:${devPort}/bundle.js"></script>\n${match}`,
+		}],
+		codeSync: false,
+		timestamps: false,
+		logLevel: 'silent',
+	}
 };
