cdd6958c429b9f8bdd4a8ad63e2d5076398884f8
Petter Walbø Johnsgård <petter@dekode.no>
2018-03-15 16:47:17 +0100

    Add simple e2e test

--- tasks/e2e.sh
+++ tasks/e2e.sh
@@ -7,6 +7,40 @@
 # Start in tasks/ even if run from root directory
 cd "$(dirname "$0")"
 
+original_npm_registry_url=`npm get registry`
+original_yarn_registry_url=`yarn config get registry`
+
+function cleanup {
+  echo 'Cleaning up.'
+  cd "$root_path"
+  # Uncomment when snapshot testing is enabled by default:
+  # rm ./packages/react-scripts/template/src/__snapshots__/App.test.js.snap
+  rm -rf dist
+  npm set registry "$original_npm_registry_url"
+  yarn config set registry "$original_yarn_registry_url"
+}
+
+# Error messages are redirected to stderr
+function handle_error {
+  echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  cleanup
+  echo 'Exiting with error.' 1>&2;
+  exit 1
+}
+
+function handle_exit {
+  cleanup
+  echo 'Exiting without error.' 1>&2;
+  exit
+}
+
+# Check for the existence of one or more files.
+function exists {
+  for f in $*; do
+    test -e "$f"
+  done
+}
+
 # Exit the script with a helpful error message when any error is encountered
 trap 'set +x; handle_error $LINENO $BASH_COMMAND' ERR
 
@@ -30,3 +64,16 @@ yarn
 
 # Test local build command
 yarn build
+
+# Check for expected output
+exists dist/js/*.js
+exists dist/css/*.css
+
+# Test local icon build command
+yarn icons
+
+# Check for expected output
+exists dist/icons.php
+
+# Cleanup
+cleanup
