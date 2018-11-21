f731f046f855a4adf9cd06119a208c969751e327
Petter Walbø Johnsgård <petter@dekode.no>
2018-11-21 11:18:28 +0100

    Revert

--- packages/browserslist-config/__tests__/index.js
+++ packages/browserslist-config/__tests__/index.js
@@ -17,6 +17,6 @@ it( 'should export an array', () => {
 } );
 
 it( 'should not contain invalid queries', () => {
-	const result = browserslist( [ 'extends @dekode/browserslist-config-default' ] );
+	const result = browserslist( [ 'extends browserslist-config-heisenberg' ] );
 	expect( result ).toBeTruthy();
 } );
