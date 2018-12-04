87996320e57ce89461c720a3e433b51fe14bb1b9
Petter Walbø Johnsgård <petter@dekode.no>
2018-12-04 19:05:59 +0100

    Set envirmoent variables

--- packages/heisenberg-scripts/scripts/start.js
+++ packages/heisenberg-scripts/scripts/start.js
@@ -1,12 +1,24 @@
 /**
+ * Do this as the first thing so that any code reading it knows the right env.
+ */
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+/**
+ * Makes the script crash on unhandled rejections instead of silently
+ * ignoring them.
+ */
+process.on( 'unhandledRejection', err => {
+	throw err;
+} );
+
+/**
  * External dependencies
  */
-const {
-	choosePort,
-} = require( 'react-dev-utils/WebpackDevServerUtils' );
+const { choosePort } = require( 'react-dev-utils/WebpackDevServerUtils' );
+const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
 const chalk = require( 'chalk' );
 const webpack = require( 'webpack' );
-const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
 
 /**
  * Internal dependencies
